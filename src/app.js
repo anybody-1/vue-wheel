037007426fa8f1dc052c6692a2ca189b03445350
吕双永 <1721446288@qq.com>
2019-05-08 15:56:06 +0800

    使用 npm test 和 npm run dev-test 进行自动化测试

--- src/app.js
+++ src/app.js
@@ -19,8 +19,8 @@ import chai from 'chai'
 import spies from 'chai-spies'
 chai.use(spies)
 const expect = chai.expect
-
-//单元测试
+try {
+    //单元测试
 {
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
@@ -94,7 +94,16 @@ const expect = chai.expect
     vm.$mount()
     let spy = chai.spy(function(){})
     vm.$on('click', spy)
+    //希望这个函数被执行
     let button = vm.$el
     button.click()
     expect(spy).to.have.been.called()
 }
+} catch (error) {
+    window.errors = [error]
+} finally {
+    window.errors&&window.errors.forEach(error => {
+        console.error(error.message)
+    }) 
+}
+
