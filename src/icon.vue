112e2ac68eb0cc2aefa00ddaf46b44d4076de03f
吕双永 <1721446288@qq.com>
2019-07-09 10:28:59 +0800

    添加input.vue及其样式

--- src/icon.vue
+++ src/icon.vue
@@ -4,10 +4,11 @@
 <script>
 import './svg'
 export default {
+    name: 'GuluIcon',
     props: ['name']
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
     .g-icon {
             width: 1em; height: 1em;
             vertical-align: -0.15em;
