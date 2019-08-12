f07527412412973ed4728c33ede8b377a8fe434f
吕双永 <1721446288@qq.com>
2019-08-12 18:33:34 +0800

    创建collapse,完成基本功能

---
+++ src/collapse.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'GuluCollapse',
+  props: {
+    title: String,
+    required: true
+  }
+}
+</script>
+<style lang="scss" scoped>
+.collapse {
+  $bor: #ddd;
+  $border-radius: 4px;
+  border: 1px solid $bor;
+  border-radius: $border-radius;
+}
+</style>
+
