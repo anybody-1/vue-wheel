0118363be83b03776f1dbd32b65813bf21d87aff
lvshuangyong <1721446288@qq.com>
2020-08-08 07:19:39 +0800

    增加loading显示

--- src/cascader-item.vue
+++ src/cascader-item.vue
@@ -1,112 +1,138 @@
 <template>
-    <div class="cascader-item" :style="{height:height}">
-        <div class="left">
-            <div class="label" v-for="(item,index) in items" :key="index" @click="onClickLabel(item)">
-                <span class="name">{{ item.label}}</span>
-                <span class="icons">
-                   <lv-icon name="right" v-if="rightArrowVisible(item)"></lv-icon>
-                </span>
-            </div>
-        </div>
-        <div class="right" v-if="rightItems">
-            <cascader-item :items="rightItems" :level="level+1" :load="load" :selected="selected"
-                           @update:selected="updateSelected"></cascader-item>
-        </div>
+  <div class="cascader-item" :style="{height:height}">
+    <div class="left">
+      <div class="label" v-for="(item,index) in items" :key="index" @click="onClickLabel(item)">
+        <span class="name">{{ item.label}}</span>
+        <span class="icons">
+          <template v-if="item.label === loadingItem.label&&!item.children">
+            <icon class="loading" name="loading"></icon>
+          </template>
+          <template v-else>
+            <icon class="next" v-if="rightArrowVisible(item)" name="right"></icon>
+          </template>
+        </span>
+      </div>
     </div>
+    <div class="right" v-if="rightItems">
+      <cascader-item
+        ref="right"
+        :items="rightItems"
+        :level="level+1"
+        :load="load"
+        :loading-item="loadingItem"
+        :selected="selected"
+        :height="height"
+        @update:selected="updateSelected"
+      ></cascader-item>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "CascaderItem",
-        props: {
-            items: {
-                type: Array
-            },
-            height: {
-                type: String
-            },
-            selected: {
-                type: Array
-            },
-            level: {
-                type: Number,
-                default: 0
-            },
-            load: {
-                type: Function
-            }
-        },
-        data() {
-            return {};
-        },
-        methods: {
-            rightArrowVisible(item) {
-                return item.load ? '' : item.children
-            },
-            updateSelected(newval) {
-                console.log(newval)
-                this.$emit('update:selected', newval)
-            },
-            onClickLabel(item) {
-                let selected = JSON.parse(JSON.stringify(this.selected))
-                selected[this.level] = item
-                selected.splice(this.level + 1)
-                this.$emit('update:selected', selected)
-            }
-        },
-        computed: {
-
-            rightItems() {
-                if (this.selected[this.level]) {
-                    let item = this.items.filter(e => e.label === this.selected[this.level].label)
-                    if (item && item[0].children && item[0].children.length > 0) {
-                        return item[0].children
-                    }
-                }
-            }
+import Icon from "./icon";
+export default {
+  name: "CascaderItem",
+  components: { Icon },
+  props: {
+    items: {
+      type: Array,
+    },
+    height: {
+      type: String,
+    },
+    loadingItem: {
+      type: Object,
+      default: () => ({}),
+    },
+    selected: {
+      type: Array,
+    },
+    level: {
+      type: Number,
+      default: 0,
+    },
+    load: {
+      type: Function,
+    },
+  },
+  methods: {
+    upDateSource() {},
+    rightArrowVisible(item) {
+      return this.load ? !item.isLeaf : item.children;
+    },
+    updateSelected(newval) {
+      this.$emit("update:selected", newval);
+    },
+    onClickLabel(item) {
+      let selected = JSON.parse(JSON.stringify(this.selected));
+      selected[this.level] = item;
+      selected.splice(this.level + 1);
+      this.$emit("update:selected", selected);
+    },
+  },
+  computed: {
+    rightItems() {
+      if (this.selected[this.level]) {
+        let item = this.items.filter(
+          (e) => e.label === this.selected[this.level].label
+        );
+        if (item && item[0].children && item[0].children.length > 0) {
+          return item[0].children;
         }
-    };
+      }
+    },
+  },
+  created() {
+    console.log("查看数据");
+    console.log(this.items);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    @import "global";
+@import "global";
 
-    .cascader-item {
-        display: flex;
-        justify-content: flex-start;
-        align-items: flex-start;
-        height: 200px;
-        overflow: auto;
+.cascader-item {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: 200px;
+  overflow: auto;
 
-        .left {
-            height: 100%;
-            overflow: auto;
+  .left {
+    height: 100%;
+    overflow: auto;
 
-            .label {
-                padding: .5em 1em;
-                display: flex;
-                cursor: pointer;
-                white-space: nowrap;
+    .label {
+      padding: 0.5em 1em;
+      display: flex;
+      cursor: pointer;
+      white-space: nowrap;
 
-                &:hover {
-                    background-color: $grey;
-                }
+      &:hover {
+        background-color: $grey;
+      }
 
-                .name {
-                    margin-right: 1em;
-                    user-select: none;
-                }
+      .name {
+        margin-right: 1em;
+        user-select: none;
+      }
 
-                .icons {
-                    margin-left: auto;
-                    transform: scale(0.5);
-                }
-            }
+      .icons {
+        margin-left: auto;
+        .next {
+          transform: scale(0.5);
         }
-
-        .right {
-            height: 100%;
-            border-left: 1px solid $border-color-light;
+        .loading {
+          animation: spin 1s infinite linear;
         }
+      }
     }
+  }
+
+  .right {
+    height: 100%;
+    border-left: 2px solid $border-color-light;
+  }
+}
 </style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,91 +1,144 @@
 <template>
-    <div class="cascader" ref="cascader" v-click-outside="close">
-        <div class="result" @click="toggle">{{ result }}</div>
-        <div class="cascader-body" v-if="cascaderVisible">
-            <cascader-item :items="source" :selected="selected" @update:selected="updateSelected"
-                           :load="load"></cascader-item>
-        </div>
+  <div class="cascader" ref="cascader" v-click-outside="close">
+    <div class="result" @click="toggle">{{ result }}</div>
+    <div class="cascader-body" v-if="cascaderVisible">
+      <cascader-item
+        :items="source"
+        :selected="selected"
+        :height="popoverHeight"
+        :loading-item="loadingItem"
+        @update:selected="updateSelected"
+        :load="load"
+      ></cascader-item>
     </div>
+  </div>
 </template>
 
 <script>
-    import ClickOutside from "./click-outside";
-    import CascaderItem from "./cascader-item";
+import ClickOutside from "./click-outside";
+import CascaderItem from "./cascader-item";
 
-    export default {
-        name: "cascader",
-        components: {CascaderItem},
-        props: {
-            source: {
-                type: Array
-            },
-            selected: {
-                type: Array,
-                default: () => []
-            },
-            load: {
-                type: Function
-            }
-        },
-        data() {
-            return {
-                cascaderVisible: false,
-                items: []
-            };
-        },
-        directives: {
-            ClickOutside
-        },
-        computed: {
-            result() {
-                return this.selected.map(e => e.label).join('/')
-            }
-        },
-        methods: {
-            updateSelected(newVal) {
-                this.$emit('update:selected', newVal)
-            },
-            close() {
-                this.cascaderVisible = false;
-            },
-            open() {
-                this.cascaderVisible = true;
-            },
-            toggle() {
-                if (this.cascaderVisible === true) {
-                    this.close();
-                } else {
-                    this.open();
-                }
-            }
-        }
+export default {
+  name: "cascader",
+  components: { CascaderItem },
+  props: {
+    source: {
+      type: Array,
+    },
+    popoverHeight: {
+      type: String,
+    },
+    selected: {
+      type: Array,
+      default: () => [],
+    },
+    load: {
+      type: Function,
+    },
+  },
+  data() {
+    return {
+      cascaderVisible: false,
+      loadingItem: {},
     };
+  },
+  directives: {
+    ClickOutside,
+  },
+  computed: {
+    result() {
+      return this.selected.map((e) => e.label).join("/");
+    },
+  },
+  methods: {
+    updateSelected(newVal) {
+      this.$emit("update:selected", newVal);
+      let lastItem = newVal[newVal.length - 1];
+      let simplest = (children, id) => {
+        return children.filter((item) => item.id === id)[0];
+      };
+      let complex = (children, id) => {
+        let noChildren = [];
+        let hasChildren = [];
+        children.forEach((item) => {
+          if (item.children) {
+            hasChildren.push(item);
+          } else {
+            noChildren.push(item);
+          }
+        });
+        let found = simplest(noChildren, id);
+        if (found) {
+          return found;
+        } else {
+          found = simplest(hasChildren, id);
+          if (found) {
+            return found;
+          } else {
+            for (let i = 0; i < hasChildren.length; i++) {
+              found = complex(hasChildren[i].children, id);
+              if (found) {
+                return found;
+              }
+            }
+            return undefined;
+          }
+        }
+      };
+      let updateSource = (result) => {
+        this.loadingItem = {};
+        let copy = JSON.parse(JSON.stringify(this.source));
+        let toUpdate = complex(copy, lastItem.id);
+        toUpdate.children = result;
+        this.$emit("update:source", copy);
+      };
+      if (!lastItem.isLeaf && this.load) {
+        this.load(lastItem, updateSource); // 回调:把别人传给我的函数调用一下
+        // 调回调的时候传一个函数,这个函数理论应该被调用
+        this.loadingItem = lastItem;
+      }
+    },
+    close() {
+      this.cascaderVisible = false;
+    },
+    open() {
+      this.cascaderVisible = true;
+    },
+    toggle() {
+      if (this.cascaderVisible === true) {
+        this.close();
+      } else {
+        this.open();
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    @import "global";
+@import "global";
 
-    .cascader {
-        display: inline-block;
-        position: relative;
+.cascader {
+  display: inline-block;
+  position: relative;
 
-        .result {
-            min-width: 10em;
-            height: $input-height;
-            border: 1px solid $border-color;
-            border-radius: $border-radius;
-            display: inline-flex;
-            align-items: center;
-            padding: 0.1em 0.5em;
-        }
+  .result {
+    min-width: 10em;
+    height: $input-height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    display: inline-flex;
+    align-items: center;
+    padding: 0.1em 0.5em;
+  }
 
-        .cascader-body {
-            position: absolute;
-            background: white;
-            margin-top: 5px;
-            display: flex;
-            z-index: 1;
-            @extend .box-shadow;
-        }
-    }
+  .cascader-body {
+    position: absolute;
+    background: white;
+    margin-top: 5px;
+    display: flex;
+    z-index: 1;
+    @extend .box-shadow;
+  }
+}
 </style>>
